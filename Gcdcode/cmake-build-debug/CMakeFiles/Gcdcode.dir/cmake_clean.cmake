b92c62865464e4b2f236cc8574b3bd34eb6a81dc
00abdoreda <56522413+00abdoreda@users.noreply.github.com>
2022-12-24 19:14:16 +0200

    Add files via upload

---
+++ Gcdcode/cmake-build-debug/CMakeFiles/Gcdcode.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Gcdcode.dir/main.cpp.obj"
+  "Gcdcode.exe"
+  "Gcdcode.exe.manifest"
+  "Gcdcode.pdb"
+  "libGcdcode.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Gcdcode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
